2eb40a7d018e7a3bf820923bbe5a3e0fbf6fa065
Yorji Perez <havsoftox@gmail.com>
2014-07-23 17:29:12 -0700

    test 4

--- public/js/advocate_actions.js
+++ public/js/advocate_actions.js
@@ -1,22 +1,5 @@
 $(document).ready(function() {
 
-    $("#advocate_referrer").autocomplete({
-        source: function(request, response) {
-            var request = $.ajax({
-                type: "POST",
-                url: 'ajax/manage_advocate_ajax.php?method=searchAdvocateReferer',
-                data: {'data': {'email': request.term}}
-            });
-            request.done(function(data, status, xhr) {
-                var data = jQuery.parseJSON(data);
-                response(data);
-            });
-        },
-        focus: function() {
-            return false;
-        }
-    });
-
     $('#btn_create_referral').click(function(e) {
         e.preventDefault();
         var isValid = validateCreateReferral();
